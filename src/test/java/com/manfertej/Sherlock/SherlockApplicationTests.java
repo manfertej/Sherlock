59863772b7b488888e4f8375e2001a81a08f5184
Manuel <manfertej@gmail.com>
2024-07-09 01:50:51 +0200

    Initial commit

---
+++ src/test/java/com/manfertej/Sherlock/SherlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manfertej.Sherlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
