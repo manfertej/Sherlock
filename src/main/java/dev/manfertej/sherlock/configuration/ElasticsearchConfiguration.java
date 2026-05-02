51e78a2208b47578966045855c84f372bcebe3a3
manuelFdz <manfertej@gmail.com>
2026-05-02 18:33:33 +0200

    Elastic connection

--- src/main/java/dev/manfertej/sherlock/configuration/ElasticsearchConfiguration.java
+++ src/main/java/dev/manfertej/sherlock/configuration/ElasticsearchConfiguration.java
@@ -1,8 +1,34 @@
 package dev.manfertej.sherlock.configuration;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+import java.net.URISyntaxException;
+
 
 @Configuration
 public class ElasticsearchConfiguration {
 
+  @Value("${elasticsearch.url}")
+  private String URL;
+
+  @Bean
+  public ElasticsearchClient elasticsearchClient() throws URISyntaxException {
+
+      RestClient restClient = RestClient.builder(HttpHost.create("localhost:9200"))
+              .build();
+
+      ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+      return new ElasticsearchClient(transport);
+
+  }
 }
--- src/main/java/dev/manfertej/sherlock/controller/IndexController.java
+++ src/main/java/dev/manfertej/sherlock/controller/IndexController.java
@@ -1,8 +1,48 @@
 package dev.manfertej.sherlock.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.scope.ScopedProxyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
+@RequestMapping("/index")
+@RequiredArgsConstructor
 public class IndexController {
+
+
+    private final ElasticsearchClient client;
+
+    @PostMapping("/file")
+    public ResponseEntity indexFile(@RequestParam("catalog") MultipartFile file) {
+
+        if(file.isEmpty())
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("File is Empty");
+
+
+        return null;
+    }
+
+
+    @PostMapping("/creteIndex")
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity createIndex() throws IOException {
+        
+
+        CreateIndexResponse response = client.indices().create(builder -> builder
+                .index("products"));
+
+        return null;
+    }
 
 }
